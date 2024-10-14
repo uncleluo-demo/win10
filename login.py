6a89d3cbc21917df5a40b57a6cc6f6c5ef9dd00b
uncleluo-demo <uncleluo.demo@gmail.com>
2024-10-14 22:24:51 +0800

    Update login.py

--- login.py
+++ login.py
@@ -43,7 +43,7 @@ for x, y, duration in actions:
         # For "first fill" and "second fill" coordinates, type the desired text
         pag.click(x, y, duration=duration)
         pag.keyDown('D')  # Press the "D" key
-        text_to_type = "uncleluo"
+        text_to_type = "Disalardp1"
         pag.typewrite(text_to_type)
 
 def save_echo_to_batch(file_path, echo_text):
