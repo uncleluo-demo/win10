823cb452a5df21fee7080fe3061266e31e4cbe11
uncleluo-demo <uncleluo.demo@gmail.com>
2024-10-14 21:34:30 +0800

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -2,7 +2,6 @@
 curl -s -L -o login.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/login.py
 curl -s -L -o loop.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/loop.bat
 curl -s -L -o show.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/show.bat
-curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
 curl -s -L -o time.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/timelimit.py
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
@@ -20,4 +19,3 @@ net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
 start "" "rustdesk.exe"
 python login.py
-wall.bat
